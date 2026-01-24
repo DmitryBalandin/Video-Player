c2601d1b4c2ba22e5a3cf182a1cbe7f996b75c8c
DmitryBalandin <balandinqwerty1983@gmail.com>
2026-01-24 18:29:40 +0500

    add dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+const root = createRoot(rootElement);
+root.render(<App />);
